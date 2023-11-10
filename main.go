e700aad03c0a7aba151a5c38911ca1db23f8b5e5
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-09 19:54:07 -0600

    feat: Implement longest common subsequence algorithm
    This is the basis for the diff algorithm, and it is what I will be using
    to determine whether a string exists in a file.

--- main.go
+++ main.go
@@ -4,6 +4,25 @@ import (
 	"fmt"
 )
 
+func longestCommonSubsequence(a, b []string) [][]int {
+	lenA, lenB := len(a), len(b)
+	dp := make([][]int, lenA+1)
+	for i := range dp {
+		dp[i] = make([]int, lenB+1)
+	}
+
+	for i := 1; i <= lenA; i++ {
+		for j := 1; j <= lenB; j++ {
+			if a[i-1] == b[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp
+}
+
 func main() {
 	filename := "target/computer.go"
 	needle := `TestString`
