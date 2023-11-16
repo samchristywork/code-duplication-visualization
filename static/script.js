bf6f17895b9e33e501356b0b08c4f2cf4c5ec52b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-15 20:48:21 -0600

    style: Fix whitespace style
    The Javascript code was not properly formatted. This mistake has been
    fixed by my auto formatter!

--- static/script.js
+++ static/script.js
@@ -1,23 +1,24 @@
-  fetch(`/files`)
-    .then(response => response.json())
-    .then(data => {
-      let files = data;
+fetch(`/files`)
+  .then(response => response.json())
+  .then(data => {
+    let files = data;
 
-      let target = document.getElementById('target');
-      for (let file of files) {
-        let panel = document.createElement('div');
-        panel.className = 'panel';
-        target.appendChild(panel);
-        fetch(`/file?file=${file}`)
-          .then(response => response.json())
-          .then(data => {
-            for (let line of data) {
-              let span = document.createElement('span');
-              span.className = 'line';
-              span.innerText = line.Line;
-              span.style.color = `rgba(${line.Color.R}, ${line.Color.G}, ${line.Color.B}, ${line.Color.A})`;
-              panel.appendChild(span);
-            }
-          });
-      }
-    });
+    let target = document.getElementById('target');
+    for (let file of files) {
+      let panel = document.createElement('div');
+      panel.className = 'panel';
+      target.appendChild(panel);
+      fetch(`/file?file=${file}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          for (let line of data) {
+            let span = document.createElement('span');
+            span.className = 'line';
+            span.innerText = line.Line;
+            span.style.color = `rgba(${line.Color.R}, ${line.Color.G}, ${line.Color.B}, ${line.Color.A})`;
+            panel.appendChild(span);
+          }
+        });
+    }
+  });
