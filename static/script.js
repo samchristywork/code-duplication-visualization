88ffd399ac9294b0a9907fe4507a29ea116aedbf
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-11-22 23:09:30 -0600

    refactor: Move shared code to a new file
    This code will be used across pages and should be in its own file.

--- static/script.js
+++ static/script.js
@@ -1,31 +1,5 @@
 let tooltip = document.getElementById('tooltip');
 
-function makeLine(line) {
-  let span = document.createElement('span');
-
-  span.className = 'line';
-  span.innerText = line.Line + '\n';
-  span.style.color = `rgba(${line.Color.R}, ${line.Color.G}, ${line.Color.B}, ${line.Color.A})`;
-
-  if (line.Tooltip) {
-    span.addEventListener('mouseover', () => {
-      tooltip.innerText = line.Tooltip;
-      tooltip.style.display = 'block';
-    });
-
-    span.addEventListener('mouseout', () => {
-      tooltip.style.display = 'none';
-    });
-
-    span.addEventListener('mousemove', (e) => {
-      tooltip.style.left = `${e.clientX + 10}px`;
-      tooltip.style.top = `${e.clientY + 10}px`;
-    });
-  }
-
-  return span;
-}
-
 fetch(`/files`)
   .then(response => response.json())
   .then(data => {
